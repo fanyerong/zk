77cdaf4f26ff3d876dab0a30ff681024b386a47c
fanyerong.html <1763645518@qq.com>
2016-11-18 11:07:19 +0800

    范叶荣

--- app/scripts/controllers/abc.js
+++ app/scripts/controllers/abc.js
@@ -11,7 +11,7 @@ angular.module('yoApp')
   .directive('auto',function($http){
   	return{
   		restrict:'ECMA',
-  		template:'<div><ul><li ng-repeat="a in aa">{{a.title | limitTo:5}}{{a.title.length>5?".....":""}}</li><p ng-repeat="b in bb"><img ng-src="{{b.img}}" alt="..."></p></ul></div>',
+  		template:'<div><ul><li ng-repeat="a in aa">{{a.title | u}}</li><p ng-repeat="b in bb"><img ng-src="{{b.img}}" alt="..."></p></ul></div>',
   		replace:true,
   		scope:true,
   		link:function(s,e,attr){
@@ -33,27 +33,12 @@ angular.module('yoApp')
   	}
   	
   })
-  .directive('auto1',function($http){
-  	return{
-  		restrict:'ECMA',
-  		template:'<div><ul><li ng-repeat="a in aa">{{a.title | limitTo:5}}{{a.title.length>5?".....":""}}</li></ul><p ng-repeat="b in bb"><img ng-src="{{b.img}}" alt="..."></div>',
-  		replace:true,
-  		scope:true,
-  		link:function(s,e,attr){
-	  	$http({
-	  		url:'http://www.somenote.cn:1510/test2',
-	  		method:'get'
-	  	}).success(function(e){
-	  		s.aa=e
-	  	}),
-	  	$http({
-	  		url:'http://www.somenote.cn:1510/aut',
-	  		method:'get'
-	  	}).success(function(e){
-	  		s.bb=e
-	  	})
-	  	
-  		}
-  	}
-  	
-  })
+  .filter('u',function(){
+		return function(input){
+			if(input.length>=10){
+				return input.substr(0,6)+"......"
+			}else{
+				return input;
+			}
+		}
+});
